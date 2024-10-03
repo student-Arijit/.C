470e5be3212253aa39d63e85a5a3b98ee46f4a11
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2024-10-03 07:45:47 +0000

    linked list

--- linked-list/1BasicRepSinglyList.c
+++ linked-list/1BasicRepSinglyList.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+struct node {
+    int data;
+    struct node *next;
+};
 
+int main(void) {
+    struct node *ptr;
+    struct node *one = NULL;
+    struct node *two = NULL;
+    struct node *three = NULL;
+
+    one = malloc(sizeof(struct node));
+    two = malloc(sizeof(struct node));
+    three = malloc(sizeof(struct node));
+
+    one -> data = 1;
+    two -> data = 2;
+    three -> data = 3;
+
+    one -> next = two;
+    two -> next = three;
+    three -> next = NULL;
+
+    ptr = one;
+
+    while (ptr != NULL) {
+        printf("%d ", ptr -> data);
+        ptr = ptr -> next;
+    }
     
     return 0;
 }
