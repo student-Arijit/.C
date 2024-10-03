d7a0c225a3e6ad5354ebeacc63c6130f6c9bc87d
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2024-10-03 04:27:10 +0000

    upgrade

--- Structure/Q1.c
+++ Structure/Q1.c
@@ -1,6 +1,7 @@
 //Write a program using Structures to read and display the information about a student
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct STUDENT {
     int roll;
@@ -11,26 +12,51 @@ void Get_Data(struct STUDENT*);
 void Print_Data(struct STUDENT*);
 
 int main(void) {
-    struct STUDENT std[10];
+    for (;;) {
+        struct STUDENT std[10];
+        int toggle;
 
-    Get_Data(std);
+        printf("\n1. Enter a detail of student\n2. Display saved detail\n3. Stop\nEnter your choice: ");
+        scanf("%d", &toggle);
 
-    Print_Data(std);
+        switch(toggle) {
+            case 1: {
+                if (sizeof(std) == 10) {
+                    printf("Memory full");
+                } else {
+                    Get_Data(std);
+                }
+            } break;
+
+            case 2: {
+                Print_Data(std);
+            } break;
+
+            case 3: {
+                exit(0);
+            } break;
+
+            default: {
+                printf("You entered wrong!!");
+            }
+        }
+
+    }
 
     return 0;
 }
 
-void Get_Data(struct STUDENT Data[]) {
-    for (size_t i = 0; i < 1; i++) {
+void Get_Data(struct STUDENT *Data) {
+
         printf("Enter your name:");
-        scanf("%s", Data[i].name);
+        scanf("%s", Data -> name);
         printf("Enter your roll");
-        scanf("%d", &Data[i].roll);
-    }
+        scanf("%d", &Data -> roll);
+    
 }
 
 void Print_Data(struct STUDENT Data[]) {
-    for (size_t i = 0; i < 1; i++) {
+    for (size_t i = 0; i < sizeof(Data); i++) {
         printf("your name: %s", Data[i].name);
         printf("your roll %d", Data[i].roll);
     }
